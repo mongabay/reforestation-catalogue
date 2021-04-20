a5b1f7ab2f1d8e9f57773b53bdaa2b2a43e2031c
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2021-04-20 12:01:47 +0200

    progress on Typescript migration for Project Chart component

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,11 @@ export interface Project {
   id: string;
   name: string;
 }
+
+export enum Category {
+  Context = 'Context',
+  Ecological = 'Ecological',
+  Economic = 'Economic',
+  Social = 'Social',
+  Institutional = 'Institutional',
+}
