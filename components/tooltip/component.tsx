7a21e7978dc326afd37497d04f2294ddd598b452
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2021-04-23 10:19:54 +0200

    typescript improvements for tooltip component

--- components/tooltip/component.tsx
+++ components/tooltip/component.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import ReactTippy from '@tippy.js/react';
 
 import './style.scss';
+import { TooltipProps } from './types';
 
-const Tooltip = props => {
+const Tooltip: React.FC<TooltipProps> = (props: TooltipProps) => {
   const mergeProps = {
     interactive: true,
     placement: 'top',
@@ -18,8 +18,5 @@ const Tooltip = props => {
 
   return <ReactTippy {...mergeProps}>{children}</ReactTippy>;
 };
-
-/** @type {any} */
-Tooltip.propTypes = { children: PropTypes.element.isRequired };
 
 export default Tooltip;
