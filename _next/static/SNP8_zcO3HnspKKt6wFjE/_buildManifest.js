b50108745fcdc0a4b2e9056098c76b5c7ded886e
pablopareja <pablopareja@users.noreply.github.com>
2021-04-23 13:12:39 +0000

    deploy: ed90089290129dd666a153d6fd0714d4c72da974

---
+++ _next/static/SNP8_zcO3HnspKKt6wFjE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/index":[a,b,c],"/project/[id]":[a,b,c]}}("static\u002Fchunks\u002F7980d3a5.9f44e13557ff2b991e5d.js","static\u002Fchunks\u002F75fc9c18.e8473622502914d682fd.js","static\u002Fchunks\u002Feec502245873920fe086ea9942e59555f860576b.1b124ee9b887008a53f2.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
