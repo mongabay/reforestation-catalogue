11ed423ab615b7027dc1f8a659e5e1ad5adfdb48
pablopareja <pablopareja@users.noreply.github.com>
2021-04-28 10:05:08 +0000

    deploy: 58ad72a74bc8d3f932f8e50a1ce603e8f2a8565f

---
+++ _next/static/8Fld_7Iis1-NKseLcNGJ4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/index":[a,b,c],"/project":[a,b,c]}}("static\u002Fchunks\u002F7980d3a5.0f9bd3a8feed49492c98.js","static\u002Fchunks\u002F75fc9c18.2c8103bcd1649f3d04ab.js","static\u002Fchunks\u002F837c5365ff7fd270269e353d2b1706a516877060.026466cbfeeacc2bfd7a.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/RcyMT8ijfnlh7UI8jtsid/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b,c){return {"/":[a,b,c],"/index":[a,b,c],"/project":[a,b,c]}}("static\u002Fchunks\u002F7980d3a5.9f44e13557ff2b991e5d.js","static\u002Fchunks\u002F75fc9c18.e8473622502914d682fd.js","static\u002Fchunks\u002F419a6cd765331b6aea4a62a0c4f404eefd07ac26.de2c66a339412f3972f0.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
