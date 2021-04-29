244e541345b038d737d78023c41cd6245b15a5e5
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2021-04-29 09:55:00 +0200

    add config slice

---
+++ modules/config/index.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const SLICE_NAME = 'config';
+
+export const selectAboutPageConfig = state => state[SLICE_NAME].aboutPage;
+export const selectProjectsConfig = state => state[SLICE_NAME].projectsPage;
+export const selectProjectDetailConfig = state => state[SLICE_NAME].projectDetailPage;
+
+export default configActions =>
+  createSlice({
+    name: SLICE_NAME,
+    initialState: {
+      aboutPage: {},
+      projectsPage: {},
+      projectDetailPage: {},
+    },
+    reducers: {
+      setConfig(state, action) {
+        const { aboutPage, projectsPage, projectDetailPage } = action.payload;
+        state.aboutPage = aboutPage;
+        state.projectsPage = projectsPage;
+        state.projectDetailPage = projectDetailPage;
+      },
+    },
+  });
