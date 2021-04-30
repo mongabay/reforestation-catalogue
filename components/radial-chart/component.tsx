0fee4d2aac25acf85925c45ed92cd4ccecf4875b
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2021-04-30 13:29:22 +0200

    some progress on radial chart component

---
+++ components/radial-chart/component.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import classnames from 'classnames';
+
+import { RadialChartProps } from './types';
+import { Category } from 'types';
+
+const RadialChart: React.FC<RadialChartProps> = ({ highlightedCategory }) => {
+  return (
+    <svg height="160" width="160">
+      <circle
+        className={classnames({
+          '-highlighted': highlightedCategory === Category.Context,
+        })}
+        cx="80"
+        cy="80"
+        r="77"
+        strokeDasharray={getStrokeDashArray(77, Category.Context)}
+      />
+
+      <circle
+        className={classnames({
+          '-highlighted': highlightedCategory === Category.Ecological,
+        })}
+        cx="80"
+        cy="80"
+        r="65"
+        strokeDasharray={getStrokeDashArray(65, Category.Ecological)}
+      />
+      <circle
+        className={classnames({
+          '-highlighted': highlightedCategory === Category.Economic,
+        })}
+        cx="80"
+        cy="80"
+        r="52"
+        strokeDasharray={getStrokeDashArray(52, Category.Economic)}
+      />
+      <circle
+        className={classnames({
+          '-highlighted': highlightedCategory === Category.Social,
+        })}
+        cx="80"
+        cy="80"
+        r="40"
+        strokeDasharray={getStrokeDashArray(40, Category.Social)}
+      />
+      <circle
+        className={classnames({
+          '-highlighted': highlightedCategory === Category.Institutional,
+        })}
+        cx="80"
+        cy="80"
+        r="27"
+        strokeDasharray={getStrokeDashArray(27, Category.Institutional)}
+      />
+    </svg>
+  );
+};
+
+export default RadialChart;
---
+++ components/radial-chart/types.ts
@@ -0,0 +1,3 @@
+export interface RadialChartProps {
+  highlightedCategory: string;
+}
