d4b38e4f5da1df188184c72660101e505df576f7
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2021-05-13 13:20:35 +0200

    hotfix: consider case where there are no/non-compliant URL parameters

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,18 @@ function HomePage(props) {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       // Function to decode URL params to object used from https://stackoverflow.com/questions/8648892/how-to-convert-url-parameters-to-a-javascript-object
-      const search = location.search.substring(1);
-      const queryParams = JSON.parse(
-        '{"' + search.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
-        (key, value) => {
-          return key === '' ? value : decodeURIComponent(value);
-        }
-      );
-      setInitialQuery(queryParams);
+      try {
+        const search = location.search.substring(1);
+        const queryParams = JSON.parse(
+          '{"' + search.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
+          (key, value) => {
+            return key === '' ? value : decodeURIComponent(value);
+          }
+        );
+        setInitialQuery(queryParams);
+      } catch (error) {
+        setInitialQuery({});
+      }
     }
   }, []);
 
