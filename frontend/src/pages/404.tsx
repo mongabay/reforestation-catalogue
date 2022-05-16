698a9904bdd560fdd3cd30c0c01729b07c4af875
Clément Prod'homme <contact@clementprodhomme.fr>
2022-05-16 09:52:36 +0200

    Add a basic 404 page

---
+++ frontend/src/pages/404.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Button from 'components/button';
+import Head from 'components/head';
+import LayoutContainer from 'components/layout-container';
+import { StaticPageLayoutProps } from 'layouts/static-page';
+import { PageComponent } from 'types';
+
+export const HomePage: PageComponent<{}, StaticPageLayoutProps> = () => (
+  <LayoutContainer className="py-24">
+    <Head />
+    <div className="min-h-full px-4 py-16 bg-white sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+      <div className="mx-auto max-w-max">
+        <main className="sm:flex">
+          <p className="text-4xl font-extrabold text-green sm:text-5xl">404</p>
+          <div className="sm:ml-6">
+            <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+              <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl">Page not found</h1>
+              <p className="mt-3 text-base text-gray-500">
+                Please check the URL in the address bar and try again.
+              </p>
+            </div>
+            <div className="flex mt-10 space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+              <Button to="/">Go back home</Button>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  </LayoutContainer>
+);
+
+export default HomePage;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Head from 'components/head';
 import LayoutContainer from 'components/layout-container';
-import { StaticPageLayoutProps } from 'layout/static-page';
+import { StaticPageLayoutProps } from 'layouts/static-page';
 import { PageComponent } from 'types';
 
 export const HomePage: PageComponent<{}, StaticPageLayoutProps> = () => (
