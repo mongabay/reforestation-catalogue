79f354bb5309aa1aa5d8fe7b42ccd13babfbd40f
Clément Prod'homme <contact@clementprodhomme.fr>
2022-05-30 16:19:58 +0200

    Add the glossary on the forms

--- frontend/src/pages/explore/project/[id]/edit.tsx
+++ frontend/src/pages/explore/project/[id]/edit.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { GetServerSideProps } from 'next';
 
 import { fetchProject } from 'hooks/projects';
 
 import Button from 'components/button';
+import GlossaryModal from 'components/glossary-modal';
 import Head from 'components/head';
 import Icon from 'components/icon';
 import LayoutContainer from 'components/layout-container';
@@ -29,25 +30,40 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 export const NewProjectPage: PageComponent<{ project: Project }, FormLayoutProps> = ({
   project,
-}) => (
-  <LayoutContainer className="pt-6 pb-12 sm:pb-24">
-    <Head title="Submit Project Changes" />
-    <Button
-      theme="secondary-green"
-      to={`/explore/project/${project.id}`}
-      className="sm:inline-flex"
-    >
-      <Icon icon={LeftArrowIcon} aria-hidden className="h-3 mr-2" />
-      Go back to the project page
-    </Button>
-    <div className="max-w-lg mx-auto mb-6 sm:mb-8 mt-11">
-      <h1 className="font-serif text-3xl font-bold text-center text-green">
-        Submit Project Changes
-      </h1>
+}) => {
+  const [showGlossaryModal, setShowGlossaryModal] = useState(false);
+
+  return (
+    <div className="relative pt-6 pb-12 sm:pb-24">
+      <LayoutContainer>
+        <Head title="Submit Project Changes" />
+        <GlossaryModal open={showGlossaryModal} onDismiss={() => setShowGlossaryModal(false)} />
+        <div className="fixed right-0 origin-bottom-right -rotate-90 md:absolute top-32 md:top-0">
+          <Button
+            className="rounded-b-none rounded-t-md"
+            onClick={() => setShowGlossaryModal(true)}
+          >
+            <span className="text-base font-semibold">Glossary</span>
+          </Button>
+        </div>
+        <Button
+          theme="secondary-green"
+          to={`/explore/project/${project.id}`}
+          className="sm:inline-flex"
+        >
+          <Icon icon={LeftArrowIcon} aria-hidden className="h-3 mr-2" />
+          Go back to the project page
+        </Button>
+        <div className="max-w-lg mx-auto mb-6 sm:mb-8 mt-11">
+          <h1 className="font-serif text-3xl font-bold text-center text-green">
+            Submit Project Changes
+          </h1>
+        </div>
+        <ProjectForm project={project} />
+      </LayoutContainer>
     </div>
-    <ProjectForm project={project} />
-  </LayoutContainer>
-);
+  );
+};
 
 NewProjectPage.layout = {
   Component: FormPageLayout,
--- frontend/src/pages/explore/project/new.tsx
+++ frontend/src/pages/explore/project/new.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Button from 'components/button';
+import GlossaryModal from 'components/glossary-modal';
 import Head from 'components/head';
 import LayoutContainer from 'components/layout-container';
 import ProjectForm from 'components/project-form';
 import FormPageLayout, { FormLayoutProps } from 'layouts/form';
 import { PageComponent } from 'types';
 
-export const NewProjectPage: PageComponent<{}, FormLayoutProps> = () => (
-  <LayoutContainer className="py-12 sm:py-24">
-    <Head title="Submit Project" />
-    <div className="max-w-lg mx-auto mb-6 sm:mb-8">
-      <h1 className="font-serif text-3xl font-bold text-center text-green">Submit Project</h1>
+export const NewProjectPage: PageComponent<{}, FormLayoutProps> = () => {
+  const [showGlossaryModal, setShowGlossaryModal] = useState(false);
+
+  return (
+    <div className="relative py-12 sm:py-24">
+      <LayoutContainer>
+        <Head title="Submit Project" />
+        <GlossaryModal open={showGlossaryModal} onDismiss={() => setShowGlossaryModal(false)} />
+        <div className="fixed right-0 origin-bottom-right -rotate-90 md:absolute top-32 md:top-0">
+          <Button
+            className="rounded-b-none rounded-t-md"
+            onClick={() => setShowGlossaryModal(true)}
+          >
+            <span className="text-base font-semibold">Glossary</span>
+          </Button>
+        </div>
+        <div className="max-w-lg mx-auto mb-6 sm:mb-8">
+          <h1 className="font-serif text-3xl font-bold text-center text-green">Submit Project</h1>
+        </div>
+        <ProjectForm />
+      </LayoutContainer>
     </div>
-    <ProjectForm />
-  </LayoutContainer>
-);
+  );
+};
 
 NewProjectPage.layout = {
   Component: FormPageLayout,
