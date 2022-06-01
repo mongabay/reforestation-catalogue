09fa4391448da111114284080b1815941732a9b3
Tiago Garcia <tiagojsag@gmail.com>
2022-06-01 12:26:06 +0200

    WIP

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -7,7 +7,7 @@ set :application, 'backend'
 set :repo_url, 'git@github.com:mongabay/reforestation-catalogue.git'
 set :repo_tree, 'backend'
 set :deploy_to, '/home/deploy_user/reforestation-catalogue-backend'
-set :branch, 'feature/MP-64'
+set :branch, 'feature/backend_deployment'
 
 set :linked_files, %w{config/database.yml config/master.key}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -15,8 +15,14 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/sys
 set :keep_releases, 3
 set :keep_assets, 3
 
+# set :rvm_type, :user
+set :rvm_ruby_version, '3.0.0'
+set :rvm_custom_path, '/usr/share/rvm'
+
 set :db_local_clean, true
 set :db_remote_clean, true
+
+set :init_system, :systemd
 
 namespace :deploy do
   desc 'Restart application'
@@ -29,37 +35,3 @@ namespace :deploy do
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
 end
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
