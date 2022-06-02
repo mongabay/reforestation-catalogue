fd3be38963e878c1efe91ea7e683d9c267b57337
Tiago Garcia <tiagojsag@gmail.com>
2022-06-02 14:26:49 +0200

    Set production server ip address on capistrano configs

--- backend/config/deploy/production.rb
+++ backend/config/deploy/production.rb
@@ -1,1 +1,3 @@
+server '52.71.145.103', user: 'deploy_user', roles: %w{web app db}
+set :rails_env, 'production'
 set :branch, 'main'
--- frontend/config/deploy/production.rb
+++ frontend/config/deploy/production.rb
@@ -1,1 +1,2 @@
+server '52.71.145.103', user: 'deploy_user', roles: %w{web}
 set :branch, 'main'
