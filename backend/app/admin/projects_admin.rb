a5b9639b01c945e173e2921b060a6ca0d31b51b2
Tiago Garcia <tiagojsag@gmail.com>
2022-06-20 14:18:41 +0200

    Fix issue in creating new projects from the admin panel

--- backend/app/admin/projects_admin.rb
+++ backend/app/admin/projects_admin.rb
@@ -1,17 +1,28 @@
 Trestle.resource(:projects) do
   # https://github.com/TrestleAdmin/trestle/issues/328#issuecomment-720786903
   # For form fields which generate an array of values (e.g. collection check boxes, multiple select fields, etc.), Rails adds a hidden empty input field, as otherwise it becomes impossible to select zero options (since no parameter would be passed).
-  # If you don't wish to update your own model setter to reject the blank item, you can define a custom update_instance block. 
+  # If you don't wish to update your own model setter to reject the blank item, you can define a custom update_instance block.
   #
   update_instance do |instance, attrs|
     Project::PROJECT_ENUMS.each do |project_enum|
       next if project_enum == 'organization_type'
       next unless attrs.keys.include?(project_enum)
-      
+
       attrs[project_enum.to_sym] = Array(attrs[project_enum.to_sym]).reject(&:blank?)
     end
 
     instance.assign_attributes(attrs)
+  end
+  build_instance do |attrs|
+    Project::PROJECT_ENUMS.each do |project_enum|
+      next if project_enum == 'organization_type'
+      next unless attrs.keys.include?(project_enum)
+
+      attrs[project_enum.to_sym] = Array(attrs[project_enum.to_sym]).reject(&:blank?)
+    end
+
+    instance = Project.new(attrs)
+    instance
   end
 
   menu do
@@ -46,7 +57,7 @@ Trestle.resource(:projects) do
     text_field :lead_organization
     resource_organization_types = Project.organization_types.keys.map { |organization_type| [organization_type.humanize, organization_type] }
     select :organization_type, resource_organization_types, { label: "Organization types" }
-    
+
     text_field :project_org_url
     text_field :partner_name
     text_field :start_year
@@ -81,19 +92,19 @@ Trestle.resource(:projects) do
 
     resource_forest_types = Project.forest_types.keys.map { |forest_type| [forest_type.humanize, forest_type] }
     select :forest_type, resource_forest_types, { label: "Forest types" }, multiple: true
-    
+
     resource_approaches = Project.approaches.keys.map { |approach| [approach.humanize, approach] }
     select :approach, resource_approaches, { label: "Approaches" }, multiple: true
-    
+
     resource_primary_objective_purposes = Project.primary_objective_purposes.keys.map { |primary_objective_purpose| [primary_objective_purpose.humanize, primary_objective_purpose] }
     select :primary_objective_purpose, resource_primary_objective_purposes, { label: "Primary objective purposes" }, multiple: true
-    
+
     resource_financial_models = Project.financial_models.keys.map { |financial_model| [financial_model.humanize, financial_model] }
     select :financial_model, resource_financial_models, { label: "Financial models" }, multiple: true
-    
+
     resource_financial_models = Project.financial_models.keys.map { |financial_model| [financial_model.humanize, financial_model] }
     select :financial_model, resource_financial_models, { label: "Financial models" }, multiple: true
-    
+
     resource_type_of_follow_ups = Project.type_of_follow_ups.keys.map { |type_of_follow_up| [type_of_follow_up.humanize, type_of_follow_up] }
     select :type_of_follow_up, resource_type_of_follow_ups, { label: "Type of follow up" }, multiple: true
   end
