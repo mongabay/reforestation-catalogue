22a13ab8c6973d58d446ac9430f53e4987fd38ed
Clément Prod'homme <contact@clementprodhomme.fr>
2022-10-10 09:29:04 +0200

    Add utils to send GA events

---
+++ frontend/src/utils/analytics.ts
@@ -0,0 +1,11 @@
+export const logEvent = (name: string, parameters: Record<string, any> = {}) => {
+  if (!window['gtag']) {
+    return;
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    window['gtag'](name, parameters);
+  } else {
+    console.info(`[GA] Event: ${name}, ${JSON.stringify(parameters, null, 2)}`);
+  }
+};
